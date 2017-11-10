0ba7ec87f9b39d149345571f6a81afd126d75700
farzd <qasimf@tcd.ie>
2017-11-10 11:09:18 +0000

    Added test suite to fail

--- test.js
+++
@@ -1,7 +0,0 @@
-var assert = require('assert');
-
-describe('Array', function() {
-    it('should equal 1', function() {
-      assert.equal(1,1, 'it is one');
-    });
-});
---
+++ test/test-spec.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+
+describe('Array', function() {
+    
+    it('should equal 1', function() {
+        assert.equal(1,1, 'it is one');
+    });
+    
+    it('should equal 0', function() {
+        assert.equal(1,0, 'it is zero');
+    });
+
+});
